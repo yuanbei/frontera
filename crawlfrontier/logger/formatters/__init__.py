4b873d8b6967d641ab955664c043e83fba8aec61
Viktor Shlapakov <vshlapakov@gmail.com>
2014-12-10 11:08:30 +0300

    Passive imports for dash mode (sqlalchemy,tldextract,colorlog)

--- crawlfrontier/logger/formatters/__init__.py
+++ crawlfrontier/logger/formatters/__init__.py
@@ -1,3 +1,9 @@
 from text import DETAILED, SHORT
-from color import (COLORED_EVENTS, COLORED_CONSOLE, COLORED_CONSOLE_MANAGER,
-                   COLORED_CONSOLE_BACKEND, COLORED_CONSOLE_DEBUGGING)
+
+try:
+    import color
+except ImportError:
+    pass
+else:
+    from color import COLORED_EVENTS, COLORED_CONSOLE, COLORED_CONSOLE_MANAGER
+    from color import COLORED_CONSOLE_BACKEND, COLORED_CONSOLE_DEBUGGING
--- crawlfrontier/utils/graphs/__init__.py
+++ crawlfrontier/utils/graphs/__init__.py
@@ -1,4 +1,9 @@
-import data
-from manager import CrawlGraphManager as Manager
-from models import CrawlPage as Page
-from models import CrawlPageRelation as Relation
+try:
+    import sqlalchemy
+except ImportError:
+    pass
+else:
+    import data
+    from manager import CrawlGraphManager as Manager
+    from models import CrawlPage as Page
+    from models import CrawlPageRelation as Relation
